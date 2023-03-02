97f0e546e8f3b79661296689e9f1dbd17085e600
Maria <mashagi@yandex.ru>
2023-03-02 12:04:33 +0300

    header layout. api-request for searching location

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,58 @@
+<template>
+  <Teleport to="body">
+    <transition name="modal-outer">
+      <div
+        v-show="modalActive"
+        class="absolute bg-black bg-opacity-30 h-screen w-screen top-0 left-0 flex justify-center px-8"
+      >
+        <transition name="modal-inner">
+          <div
+            v-if="modalActive"
+            class="p-4 bg-white self-start mt-32 max-w-screen-md"
+          >
+            <slot></slot>
+            <button
+              @click="$emit('closeModal')"
+              class="text-white mt-8 bg-weather-primary py-2 px-6"
+            >
+              Close
+            </button>
+          </div>
+        </transition>
+      </div>
+    </transition>
+  </Teleport>
+</template>
+
+<script setup>
+defineEmits(["closeModal"]);
+defineProps({
+  modalActive: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+  opacity: 0;
+}
+.modal-inner-enter-active{
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+.modal-inner-leave-active{
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+.modal-enner-enter-from{
+  opacity: 0;
+  transform: scale(0.8);
+}
+.modal-enner-leave-to {
+  transform: scale(0.8);
+}
+</style>
